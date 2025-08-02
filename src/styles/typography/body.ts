05c5e7b8b8ecf85977a5cb2b2afd252fd4d07af6
bbqcream <imground11@gmail.com>
2025-08-02 21:52:56 +0900

    feat :: community

--- src/styles/typography/body.ts
+++ src/styles/typography/body.ts
@@ -3,17 +3,17 @@ import { TextStyle } from 'react-native';
 export const body2: TextStyle = {
   fontFamily: 'Pretendard Variable',
   fontSize: 12,
-  fontWeight: 'medium',
+  fontWeight: '500',
 };
 
 export const body1: TextStyle = {
   fontFamily: 'Pretendard Variable',
   fontSize: 14,
-  fontWeight: 'medium',
+  fontWeight: '500',
 };
 
 export const body3: TextStyle = {
   fontFamily: 'Pretendard Variable',
   fontSize: 10,
-  fontWeight: 'medium',
+  fontWeight: '500',
 };
