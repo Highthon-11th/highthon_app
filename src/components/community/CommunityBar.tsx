229ba78dbc155623ce208f6747f3d2aa3b906895
bbqcream <imground11@gmail.com>
2025-08-03 00:16:04 +0900

    refactor :: questionbar

--- src/components/community/CommunityBar.tsx
+++ src/components/community/CommunityBar.tsx
@@ -1,16 +1,17 @@
 import { COLOR } from '@/styles/color/color';
 import { body1, body3 } from '@/styles/typography/body';
-import { title1, title3 } from '@/styles/typography/title';
+import { title3 } from '@/styles/typography/title';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 type BarProps = {
   title: string;
   tag: string[];
-  role: string;
-  author: string;
-  date: string;
-  view: number;
+  role?: string;
+  author?: string;
+  date?: string;
+  view?: number;
+  type: 'info' | 'question';
 };
 
 const CommunityBar = (props: BarProps) => {
@@ -18,7 +19,31 @@ const CommunityBar = (props: BarProps) => {
     <TouchableOpacity>
       <View style={styles.container}>
         <View style={styles.wrapper}>
-          <Text style={title3}>{props.title}</Text>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 4,
+            }}
+          >
+            <Text
+              style={[
+                body3,
+                {
+                  color: props.type === 'info' ? COLOR.main : '#ffffff',
+                  backgroundColor:
+                    props.type !== 'info' ? COLOR.main : COLOR.stroke,
+                  paddingVertical: 4,
+                  paddingHorizontal: 10,
+                  borderRadius: 8,
+                },
+              ]}
+            >
+              {props.type === 'info' ? '정보' : '질문'}
+            </Text>
+            <Text style={title3}>{props.title}</Text>
+          </View>
           <View style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
             {props.tag.map(items => (
               <Text style={[body1, { color: COLOR.main }]}># {items}</Text>
@@ -26,8 +51,14 @@ const CommunityBar = (props: BarProps) => {
           </View>
           <View style={{ height: 8 }} />
           <Text
-            style={body3}
-          >{`*${props.role}* ·  ${props.author} · ${props.date} · 조회 ${props.view}`}</Text>
+            style={[
+              body3,
+              { display: props.type === 'question' ? 'flex' : 'none' },
+            ]}
+          >
+            {props.type === 'question' &&
+              `*${props.role}* ·  ${props.author} · ${props.date} · 조회 ${props.view}`}
+          </Text>
         </View>
       </View>
     </TouchableOpacity>
